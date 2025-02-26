887b374ababa56fbcd8e1ca44ddf13188f3658bb
GuilleOk <fernandez.martinez1403@gmail.com>
2025-02-26 13:06:20 +0100

    arreglos en el toast

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,11 +1,25 @@
-import { useContext, useState } from "react"
+import { useContext, useEffect, useRef, useState } from "react"
 import { CartContext } from "../contexts/CartContext"
 import {WatchDescription, CartShop} from "./ModifyAmount"
 
 /* eslint-disable react/prop-types */
 const ProductCard = ({ category, description, image, price, title, id }) => {
   const [showToast, setShowToast] = useState(false)
-  const {increaseAmount} = useContext(CartContext)
+  const { increaseAmount } = useContext(CartContext)
+  const toastRef = useRef(null)
+
+  useEffect(() => {
+    const handleClickOutside = (e) => {
+      if (showToast && toastRef.current && !toastRef.current.contains(e.target)) {
+        setShowToast(false)
+      }
+    }
+
+    document.addEventListener("mousedown", handleClickOutside)
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside)
+    }
+  }, [showToast])
 
   const handleView = () => {
     setShowToast(!showToast)
@@ -29,7 +43,7 @@ const ProductCard = ({ category, description, image, price, title, id }) => {
         <button className="btn btn-outline-primary" onClick={handleView}><WatchDescription /></button>  
         <button className="btn btn-outline-primary" onClick={()=> handleIncreaseAmount({id, category})}><CartShop /></button>  
       </div>
-      <div className={showToast ? 'show toast align-items-center w-75' : 'toast align-items-center'} style={{zIndex: '900'}} role="alert" aria-live="assertive" aria-atomic="true">
+      <div ref={toastRef} className={showToast ? 'show toast align-items-center w-75' : 'toast align-items-center'} style={{zIndex: '900'}} role="alert" aria-live="assertive" aria-atomic="true">
         <div className="d-flex">
           <div className="toast-body">
             <button type="button" className="btn-close me-2 m-auto" data-bs-dismiss="toast" aria-label="Close" onClick={() => setShowToast(false)}></button>
@@ -41,7 +55,7 @@ const ProductCard = ({ category, description, image, price, title, id }) => {
                 <h2 style={{ color: 'black', textAlign: 'center' }}>{title.toUpperCase()}</h2>
                 <div style={{ color: 'black', textAlign: 'justify' }}>{description}</div>
                 <div style={{margin: '1rem .5rem', display: 'flex', justifyContent: 'space-around'}}>
-                  <button className="btn btn-primary" disabled>Price: ${price}</button>  
+                  <div style={{color: 'black', display:'flex', alignItems: 'center'}}>Price: ${price}</div>  
                   <button className="btn btn-primary" onClick={()=> handleIncreaseAmount({id, category})}><CartShop /></button>  
                 </div>
               </div>
